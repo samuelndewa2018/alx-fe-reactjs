5e07b9f146397b655671bcf54fcb058538186f9b
Samuel Githinji <samuelndewa2018@gmail.com>
2025-07-07 20:39:01 +0300

    main component updated

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import MainContent from "./components/MainContent";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
       <Header />
+      <MainContent />
       <WelcomeMessage />
       <div>
         <a href="https://vite.dev" target="_blank">
