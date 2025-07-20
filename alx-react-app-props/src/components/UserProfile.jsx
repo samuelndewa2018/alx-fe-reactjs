a730af63260090043e7d502a7d9435e4cd4fa96d
Samuel Githinji <samuelndewa2018@gmail.com>
2025-07-21 00:12:26 +0300

    user profile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,23 +1,7 @@
-const UserProfile = (props) => {
-  return (
-    <div
-      style={{
-        border: "1px solid gray",
-        padding: "15px",
-        margin: "15px",
-        borderRadius: "8px",
-        backgroundColor: "#f9f9f9",
-      }}
-    >
-      <h2 style={{ color: "blue", fontSize: "24px", marginBottom: "10px" }}>
-        {props.name}
-      </h2>
-      <p style={{ fontSize: "16px" }}>
-        Age: <span style={{ fontWeight: "bold" }}>{props.age}</span>
-      </p>
-      <p style={{ fontStyle: "italic", fontSize: "14px" }}>Bio: {props.bio}</p>
-    </div>
-  );
-};
+import UserInfo from "./UserInfo";
+
+function UserProfile() {
+  return <UserInfo />;
+}
 
 export default UserProfile;
