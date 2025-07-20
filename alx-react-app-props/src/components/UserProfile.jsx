5d07ff7b906568e09c1a520e56a5a04d90b850be
Samuel Githinji <samuelndewa2018@gmail.com>
2025-07-21 00:33:02 +0300

    user details

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,30 @@
-import UserInfo from "./UserInfo";
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
-function UserProfile() {
-  return <UserInfo />;
-}
+const UserProfile = () => {
+  const userData = useContext(UserContext);
+
+  return (
+    <div
+      style={{
+        border: "1px solid gray",
+        padding: "15px",
+        margin: "15px",
+        borderRadius: "8px",
+        backgroundColor: "#f9f9f9",
+      }}
+    >
+      <h2 style={{ color: "blue", fontSize: "24px", marginBottom: "10px" }}>
+        {userData.name}
+      </h2>
+      <p style={{ fontSize: "16px" }}>
+        Age: <span style={{ fontWeight: "bold" }}>{userData.age}</span>
+      </p>
+      <p style={{ fontStyle: "italic", fontSize: "14px" }}>
+        Bio: {userData.bio}
+      </p>
+    </div>
+  );
+};
 
 export default UserProfile;
