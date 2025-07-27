f1cfc4bbbdb99bbdaf66dc6ba84af37d3f981d10
Samuel Githinji <samuelndewa2018@gmail.com>
2025-07-27 22:24:35 +0300

    0. Initialize the Recipe Sharing Application with Zustand State Management

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.jsx
+import "./App.css";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
