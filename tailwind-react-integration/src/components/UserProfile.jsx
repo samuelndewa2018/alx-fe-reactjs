85891e56aade65af9412492c3ff71f108775c915
Samuel Githinji <samuelndewa2018@gmail.com>
2025-08-07 18:08:56 +0300

    sm corrected

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile() {
   return (
     <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
-        className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto"
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
         src="https://www.svgrepo.com/show/382109/male-avatar-boy-face-man-user-7.svg"
         alt="User"
       />
