468bbc371aa59e32be5abc5b487d35358a391f28
Samuel Githinji <samuelndewa2018@gmail.conm>
2025-08-17 10:40:43 +0300

    0. Setting Up a React Application for a Recipe Sharing Platform with Tailwind CSS

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-blue-500">
+      Recipe Sharing Platform
+    </h1>
+  );
+}
+
+export default App;
