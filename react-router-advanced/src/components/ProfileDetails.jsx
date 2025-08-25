540369c6b5e8d779483cca6739c6833f058ad308
Samuel Githinji <samuelndewa2018@gmail.com>
2025-08-25 12:00:41 +0300

    profile details page added

---
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -0,0 +1,8 @@
+export default function ProfileDetails() {
+  return (
+    <div>
+      <h3>Profile Details</h3>
+      <p>Here are your personal profile details.</p>
+    </div>
+  );
+}
---
+++ react-router-advanced/src/components/ProfileSettings.jsx
@@ -0,0 +1,8 @@
+export default function ProfileSettings() {
+  return (
+    <div>
+      <h3>Profile Settings</h3>
+      <p>Here you can change your account settings.</p>
+    </div>
+  );
+}
