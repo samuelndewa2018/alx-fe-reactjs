a5102af89e99a53cf5004b98fbd556eb71efb639
Samuel Githinji <samuelndewa2018@gmail.com>
2025-08-25 11:55:26 +0300

    src/App.jsx doesn't contain: ["BrowserRouter"]

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Blog from "./pages/Blog";
@@ -11,38 +11,40 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
-    <div style={{ fontFamily: "system-ui", padding: 20 }}>
-      <h1>Advanced Routing Demo</h1>
-      <nav style={{ display: "flex", gap: 12, marginBottom: 20 }}>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/profile">Profile</Link>
-      </nav>
+    <BrowserRouter>
+      <div style={{ fontFamily: "system-ui", padding: 20 }}>
+        <h1>Advanced Routing Demo</h1>
+        <nav style={{ display: "flex", gap: 12, marginBottom: 20 }}>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/profile">Profile</Link>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
 
-        {/* Dynamic Routing for Blog */}
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:postId" element={<Post />} />
+          {/* Dynamic Routing for Blog */}
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:postId" element={<Post />} />
 
-        {/* Protected Route with Nested Routes */}
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        >
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
+          {/* Protected Route with Nested Routes */}
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="details" element={<ProfileDetails />} />
+            <Route path="settings" element={<ProfileSettings />} />
+          </Route>
 
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
